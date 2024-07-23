b49a6fb7b7e88ccd4cb1363cd04e6b8df6d655d1
Luke Anger <anger.luke2004@gmail.com>
2024-07-22 22:26:46 -0700

    Added main function to actually run the test for version

--- tests/test_swtoolkit.py
+++ tests/test_swtoolkit.py
@@ -2,4 +2,9 @@ from swtoolkit import __version__
 
 
 def test_version():
+    print(f'the version is {__version__}')
     assert __version__ == '0.1.0'
+
+if __name__ == '__main__':
+    # testing version
+    test_version()
