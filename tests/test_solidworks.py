427b482d3d5edb970b038aae15e103b0acf88fd2
Luke Anger <anger.luke2004@gmail.com>
2024-07-25 02:41:13 -0700

    Added rotating CAD test and added a TODO for an issue in the repo

--- tests/test_solidworks.py
+++ tests/test_solidworks.py
@@ -105,9 +105,10 @@ def test_visible_set():
 )
 def test_open(filename):
     """ Test if native solidworks files can be opened """
+
     retval = SolidWorks().open(filename)
     assert retval[0]  # Returned value is true if file is opened successfully.
-    SolidWorks().close_all_documents()
+    SolidWorks().close_all_documents() # TODO: issue with parameters here
 
 
 def test_shutdown():
@@ -121,11 +122,69 @@ def test_get_model():
 def test_get_models():
     pass
 
+def test_rotating_CAD():
+    # NOTE: See the below for more details
+    # https://github.com/onuralpszr/SWPyMacros/blob/master/hello_python_world.py
+
+    import win32com
+    import pythoncom
+    import time
+    SWV = 2023 # This is solidworks version year
+    SWAV = SWV-1992 # API Version
+    
+    sw = win32com.client.Dispatch("SldWorks.Application.{}".format(SWAV)) 
+    print(f" Solidworks API Version : {SWAV}","\n",f"Solidworks Version : {SWV}")
+    Model = sw.ActiveDoc
+    ARG_NULL = win32com.client.VARIANT(pythoncom.VT_DISPATCH, None)
+    ck = Model.Extension.SelectByID2("Front", "PLANE", 0, 0, 0, False, 0, ARG_NULL, 0)
+    Model.SketchManager.InsertSketch(True)
+    Model.ClearSelection2(True)
+
+    mySketchText = Model.InsertSketchText(
+        0, 0, 0, "Hello Python World!", 0, 0, 0, 100, 100
+    )
+    myFeature = Model.FeatureManager.FeatureExtrusion2(
+        True, 
+        False,
+        False,
+        0,
+        0,
+        0.001,
+        0.001,
+        False,
+        False,
+        False,
+        False,
+        0,
+        0,
+        False,
+        False,
+        False,
+        False,
+        True,
+        True,
+        True,
+        0,
+        0,
+        False,
+    )
+
+    Model.SelectionManager.EnableContourSelection = False
+    Model.ClearSelection2(True)
+    s = time.perf_counter()
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds until while-loop")
+    time.sleep(2)
+    while True:
+        Model.ViewRotateplusy()
+        time.sleep(0.1)
+
 if __name__ == '__main__':
     # Testing visibiliity
-    print(f'the visibility is {visibility_state()}')
-    # test start
-    test_start()
+    # print(f'the visibility is {visibility_state()}')
+    # test_start()
+    test_rotating_CAD()
+    test_open('woodAndScrewCounter/testing/swtoolkit/tests/test_cad/test_asm/TEST-AAA-AA.SLDASM')
     # test kill
     # test_kill()
     # test pid
@@ -134,5 +193,3 @@ if __name__ == '__main__':
     # test_visible_get()
     # Test set visibility
     # test_visible_set()
-    # test open
-    # test_open('oops')
